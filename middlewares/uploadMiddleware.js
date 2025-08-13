58122d1d26d8d5ac7fae240a5e14d71661830155
CarlosCon9 <carlos.contreras.pardo@gmail.com>
2025-08-13 05:42:10 -0500

    Cambio de tamaño de archivos admitidos

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -20,7 +20,7 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 1024 * 1024 * 5 }, // 5MB
+    limits: { fileSize: 1024 * 1024 * 50 }, // 50MB
     fileFilter: fileFilter
 }).single('imagen');
 
